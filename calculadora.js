95e8f140d518488212cc7cc729283156f37190ec
Saulormsantos <saulormsantos@gmail.com>
2024-09-26 21:27:31 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+let a = (10);
+let b = (1);
+let operador = ("multiplicação");
+
+if (operador == "soma"){
+    console.log(a+b)
+} if (operador == "divisão")
+    console.log(a/b);
+
+        if (operador == "multiplicação")
+    console.log(a*b);
+
+            if (operador == "subtração"){
+    console.log(a-b);
+}
